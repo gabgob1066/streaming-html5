50cc4eeab41de969c484e0a213e357501856195a
rajdeeprath <rajdeeprath@gmail.com>
2017-06-01 19:49:34 +0530

    Websocket cookie stuffing
    + Added cookie stuffing before publish test : publishWithCookieExplosion
    + Yet to test the test code

---
+++ src/page/test/publishWithCookieExplosion/README.md
@@ -0,0 +1,5 @@
+# Publish with cookie explosion - websocket plugin test
+
+
+### View Your Stream
+After you have started a broadcast session, open a browser window and navigate to your Red5 Pro server (e.g., [http://localhost:5080/live/subscribe.jsp](http://localhost:5080/live/subscribe.jsp) to see a list of streams. Select the stream name used from this example and view in the browser using WebRTC, Flash or HLS playback options.
---
+++ src/page/test/publishWithCookieExplosion/index.js
@@ -0,0 +1,210 @@
+(function(window, document, red5pro, PublisherBase /* see: src/static/script/main.js */) {
+  'use strict';
+
+  var serverSettings = (function() {
+    var settings = sessionStorage.getItem('r5proServerSettings');
+    try {
+      return JSON.parse(settings);
+    }
+    catch (e) {
+      console.error('Could not read server settings from sessionstorage: ' + e.message);
+    }
+    return {};
+  })();
+
+  var configuration = (function () {
+    var conf = sessionStorage.getItem('r5proTestBed');
+    try {
+      return JSON.parse(conf);
+    }
+    catch (e) {
+      console.error('Could not read testbed configuration from sessionstorage: ' + e.message);
+    }
+    return {}
+  })();
+
+  var targetPublisher;
+  var targetView;
+
+  var updateStatusFromEvent = window.red5proHandlePublisherEvent; // defined in src/template/partial/status-field-publisher.hbs
+  var streamTitle = document.getElementById('stream-title');
+  var statisticsField = document.getElementById('statistics-field');
+
+  var protocol = serverSettings.protocol;
+  var isSecure = protocol == 'https';
+  function getSocketLocationFromProtocol () {
+    return !isSecure
+      ? {protocol: 'ws', port: serverSettings.wsport}
+      : {protocol: 'wss', port: serverSettings.wssport};
+  }
+
+  function onBitrateUpdate (bitrate, packetsSent) {
+    statisticsField.innerText = 'Bitrate: ' + Math.floor(bitrate) + '. Packets Sent: ' + packetsSent + '.';
+  }
+
+  function onPublisherEvent (event) {
+    console.log('[Red5ProPublisher] ' + event.type + '.');
+    updateStatusFromEvent(event);
+  }
+  function onPublishFail (message) {
+    console.error('[Red5ProPublisher] Publish Error :: ' + message);
+  }
+  function onPublishSuccess (publisher) {
+    console.log('[Red5ProPublisher] Publish Complete.');
+    try {
+      window.trackBitrate(publisher.getPeerConnection(), onBitrateUpdate);
+    }
+    catch (e) {
+      // no tracking for you!
+    }
+  }
+  function onUnpublishFail (message) {
+    console.error('[Red5ProPublisher] Unpublish Error :: ' + message);
+  }
+  function onUnpublishSuccess () {
+    console.log('[Red5ProPublisher] Unpublish Complete.');
+  }
+
+  function getUserMediaConfiguration () {
+    return {
+      audio: configuration.useAudio ? configuration.userMedia.audio : false,
+      video: configuration.useVideo ? configuration.userMedia.video : false,
+      frameRate: configuration.frameRate
+    };
+  }
+  
+
+
+  function determinePublisher () {
+
+    var config = Object.assign({},
+                   configuration,
+                   getUserMediaConfiguration());
+    var rtcConfig = Object.assign({}, config, {
+                      protocol: getSocketLocationFromProtocol().protocol,
+                      port: getSocketLocationFromProtocol().port,
+                      streamName: config.stream1,
+                      streamType: 'webrtc'
+                   });
+    var rtmpConfig = Object.assign({}, config, {
+                      protocol: 'rtmp',
+                      port: serverSettings.rtmpport,
+                      streamName: config.stream1,
+                      width: config.cameraWidth,
+                      height: config.cameraHeight,
+                      swf: '../../lib/red5pro/red5pro-publisher.swf',
+                      swfobjectURL: '../../lib/swfobject/swfobject.js',
+                      productInstallURL: '../../lib/swfobject/playerProductInstall.swf'
+                   });
+    var publishOrder = config.publisherFailoverOrder
+                            .split(',')
+                            .map(function (item) {
+                              return item.trim()
+                        });
+
+    return PublisherBase.determinePublisher({
+                rtc: rtcConfig,
+                rtmp: rtmpConfig
+              }, publishOrder);
+  }
+
+  function preview (publisher, requiresGUM) {
+    var elementId = 'red5pro-publisher-video';
+    var gUM = getUserMediaConfiguration();
+    return PublisherBase.preview(publisher, elementId, requiresGUM ? gUM : undefined);
+  }
+
+  function publish (publisher, view, streamName) {
+    streamTitle.innerText = streamName;
+    targetPublisher = publisher;
+    targetView = view;
+    return new Promise(function (resolve, reject) {
+      PublisherBase.publish(publisher, streamName)
+       .then(function () {
+          onPublishSuccess(publisher);
+        })
+        .catch(function (error) {
+          reject(error);
+        })
+    });
+  }
+
+  function unpublish () {
+    return new Promise(function (resolve, reject) {
+      var view = targetView;
+      var publisher = targetPublisher;
+      PublisherBase.unpublish(publisher, view)
+        .then(function () {
+          onUnpublishSuccess();
+          resolve();
+        })
+        .catch(function (error) {
+          var jsonError = typeof error === 'string' ? error : JSON.stringify(error, 2, null);
+          onUnpublishFail('Unmount Error ' + jsonError);
+          reject(error);
+        });
+    });
+  }
+  
+  
+  function stuffSomeCookies(count, domain) {	
+	var dt, expires, domain;
+	dt = new Date();
+	dt.setTime(dt.getTime()+(24*60*60*1000));
+	expires = dt.toGMTString();
+	domain = domain || window.location.host.split(':')[0];
+	console.log("Attempting to generate " + count + "cookies for domain " + domain);
+	var char, i = count || 300;
+	while(--i > -1) {
+		char = String.fromCharCode(65+(i%26));
+		document.cookie = (char + i) + 'fthingbreaksoshejusttypedandtypedandtypedandhowmuchtextodoesonereallyneedtomakehtisbreakIguesswellseesoonegouthwonatndasdjlakjsdjvlasdkjlkjfalsjdkjjfljkkkdkddddddd12345677123456781234567812345678999999999999999999999999999999999999999covfefe=' + encodeURIComponent(char + 'bar') + '; expires=' + expires + '; domain=' + domain;
+	}
+  }
+  
+  
+  function doCookieStuffing(ms) {
+	  
+	  statisticsField.innerText = 'Stuffing some large cookies into browser...';	
+      stuffSomeCookies(200);
+	  return function(x) {
+		return new Promise(resolve => setTimeout(() => function(){
+			statisticsField.innerText = '';
+			resolve(x)
+		}(), ms));
+	  };
+  }
+  
+
+  // Kick off.
+  determinePublisher()
+    .then(function (payload) {
+      var requiresPreview = payload.requiresPreview;
+      var publisher = payload.publisher;
+      publisher.on('*', onPublisherEvent);
+      return preview(publisher, requiresPreview);
+    })
+	.then(doCookieStuffing(2000))
+    .then(function (payload) {
+      var publisher = payload.publisher;
+      var view = payload.view;
+      return publish(publisher, view, configuration.stream1);
+    })
+    .catch(function (error) {
+      var jsonError = typeof error === 'string' ? error : JSON.stringify(error, null, 2);
+      console.error('[Red5ProPublisher] :: Error in publishing - ' + jsonError);
+      onPublishFail(jsonError);
+     });
+
+  window.addEventListener('beforeunload', function() {
+    function clearRefs () {
+      if (targetPublisher) {
+        targetPublisher.off('*', onPublisherEvent);
+      }
+      targetView = targetPublisher = undefined;
+    }
+    unpublish().then(clearRefs).catch(clearRefs);
+    window.untrackBitrate();
+  });
+
+})(this, document, window.red5prosdk, new window.R5ProBase.Publisher());
+
